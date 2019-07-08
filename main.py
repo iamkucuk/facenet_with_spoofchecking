ca660afa382c9a1d7add71b77c970ed6643deccd
furkan-kucuk <furkankucuk@outlook.com>
2019-07-08 22:03:31 +0300

    Further commit for face detection

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+#%%
 from facenet_pytorch import MTCNN, InceptionResnetV1
-
-mtcnn = MTCNN()
-
-resnet = InceptionResnetV1(pretrained=)
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+import numpy as np
+import pandas as pd
+from PIL import Image
+from matplotlib import pyplot as plt
+#%%
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+#%%
+mtcnn = MTCNN(device=device)
+#%%
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+#%%
+img = Image.open("dataset/face1.jpg")
+img_cropped = mtcnn(img)
+plt.imshow(np.transpose(img_cropped, (1, 2, 0)))
+plt.show()
+#%%
+img_embedding = resnet(img_cropped.unsqueeze(0).to(device)).cpu()
